f01d8e43649b49f838b30f479c0e4dd0c0583360
chetankumarnmit@gmail.com <chetankumarnmit@gmail.com>
2020-01-19 10:56:00 +0530

    test02 initila commit

---
+++ src/com/test/com/MainCtr.java
@@ -0,0 +1,18 @@
+package com.test.com;
+
+public class MainCtr {
+
+public static void main(String[] args) {
+		
+		System.out.println(" Start ...");
+		
+		for(int i =0 ;i<=5;i++) {
+			
+			System.out.println(i);
+		}
+		
+		System.out.println(" End ...");
+
+	}
+
+}
